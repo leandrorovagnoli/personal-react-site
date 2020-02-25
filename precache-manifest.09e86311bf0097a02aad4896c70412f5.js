7f99aa018ffbcf1cb1b925e6358a06295b986c7f
Leandro Rovagnoli <leandro.rovagnoli@gmail.com>
2020-02-25 04:56:47 -0300

    Updates

---
+++ precache-manifest.09e86311bf0097a02aad4896c70412f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616d03df5c263f139877c817a267b9a5",
+    "url": "/personal-react-site/index.html"
+  },
+  {
+    "revision": "844857d2d0586a24e851",
+    "url": "/personal-react-site/static/css/main.217a6724.chunk.css"
+  },
+  {
+    "revision": "3e84ab4d12e173ac694c",
+    "url": "/personal-react-site/static/js/2.176d22f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/personal-react-site/static/js/2.176d22f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844857d2d0586a24e851",
+    "url": "/personal-react-site/static/js/main.b567be4f.chunk.js"
+  },
+  {
+    "revision": "508fb8bf87f5f133ad26",
+    "url": "/personal-react-site/static/js/runtime-main.116b6fea.js"
+  },
+  {
+    "revision": "81a88614f5f23d2bc13f72e81fe6bce6",
+    "url": "/personal-react-site/static/media/user.81a88614.jpg"
+  }
+]);
