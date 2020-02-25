1c25a7641856c4097b22a27ee2f5ec4cb0157b48
Leandro Rovagnoli <leandro.rovagnoli@gmail.com>
2020-02-25 18:58:13 -0300

    Updates

---
+++ precache-manifest.8ef48e90a6b25531b5225d3560bc5c4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3123c4b3a9c65b170e46725f3988a618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7659d51856d58f53d137",
+    "url": "/static/css/main.f8e1b461.chunk.css"
+  },
+  {
+    "revision": "3e84ab4d12e173ac694c",
+    "url": "/static/js/2.176d22f9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.176d22f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7659d51856d58f53d137",
+    "url": "/static/js/main.eb87e5f0.chunk.js"
+  },
+  {
+    "revision": "4e23ad588c1d01415cd6",
+    "url": "/static/js/runtime-main.0cea152d.js"
+  },
+  {
+    "revision": "81a88614f5f23d2bc13f72e81fe6bce6",
+    "url": "/static/media/user.81a88614.jpg"
+  }
+]);
